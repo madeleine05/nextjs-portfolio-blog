b632658a6bae1e27abda033f9b170a2ddff9e422
madeleine05 <130437305+madeleine05@users.noreply.github.com>
2023-04-12 10:21:59 +0700

    Update info.tsx

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,22 +1,19 @@
-import me from '../app/avatar.jpg';
+import me from '../app/madeleine.jpg';
 
-export const name = 'Lee Robinson';
+export const name = 'Madeleine Estefany Adinugroho';
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Lee. I'm the <b>VP of Developer Experience at Vercel</b> where my
-      team helps developers build a faster web.
+      Hey, I'm Madeleine. I'm a student in SMA Karangturi.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I'm a Next.js contributor and help lead our open-source communities. I'm
-      passionate about frontend development and have created courses on React,
-      Next.js, and web development. I'm an advisor and investor in early stage
-      startups.
+      I'm a designer in an organtization in SMA Karangturi, called Karangturi
+      Magazines or KMagz. I like desgining my own websites.
     </>
   );
 };
